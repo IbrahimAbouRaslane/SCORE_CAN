2e4e7d711f105089bafd5ac56fa53a99a5fa3356
Ibrahim Nouho <IbrahimNouho123@gmail.com>
2022-01-06 16:36:15 +0000

    added somes tables

---
+++ db/migrate/20220106163150_create_predictions.rb
@@ -0,0 +1,16 @@
+class CreatePredictions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :predictions, id: :uuid do |t|
+      t.integer :score_a
+      t.integer :score_b
+      t.integer :points
+      t.integer :predicted_result
+      t.boolean :is_good_result
+      t.boolean :is_exact_score
+      t.references :match, null: false, foreign_key: true, type: :uuid
+      t.references :user, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_155408) do
+ActiveRecord::Schema.define(version: 2022_01_06_163249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -22,6 +22,36 @@ ActiveRecord::Schema.define(version: 2022_01_06_155408) do
     t.integer "team_b_id"
     t.integer "score_a"
     t.integer "score_b"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pools", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.uuid "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_pools_on_team_id"
+  end
+
+  create_table "predictions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.integer "score_a"
+    t.integer "score_b"
+    t.integer "points"
+    t.integer "predicted_result"
+    t.boolean "is_good_result"
+    t.boolean "is_exact_score"
+    t.uuid "match_id", null: false
+    t.uuid "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["match_id"], name: "index_predictions_on_match_id"
+    t.index ["user_id"], name: "index_predictions_on_user_id"
+  end
+
+  create_table "teams", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "flag_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -38,4 +68,7 @@ ActiveRecord::Schema.define(version: 2022_01_06_155408) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pools", "teams"
+  add_foreign_key "predictions", "matches"
+  add_foreign_key "predictions", "users"
 end
