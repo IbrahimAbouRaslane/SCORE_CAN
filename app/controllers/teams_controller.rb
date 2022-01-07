2d5d73b89e74a5f249d19304503581a7cfec769c
Ibrahim Nouho <IbrahimNouho123@gmail.com>
2022-01-07 18:36:51 +0000

    added teams with flag

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,57 @@
 class TeamsController < ApplicationController
+  def index
+    @teams = pool.teams
+  end
+
+  def new
+    @team = pool.teams.build
+  end
+
+  def show
+    id = params[:id]
+    @team = Team.find(id)
+  end
+
+  def create
+    @team = pool.teams.build(team_params)
+    # raise @pool.name.inspect
+    if @team.save
+      redirect_to pool_teams_path, notice: 'The team was successfully created'
+    else
+      flash[:error] = "Team has errors"
+      render 'new'
+    end
+  end
+
+  def edit
+    @team = pool.teams.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to pool_teams_path, notice: 'The team was successfully update'
+    else
+      flash[:error] = "team has errors"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    @team = Pool.find(id)
+    @pool.destroy
+    redirect_to pool_teams_path, notice: 'The team was successfully deleted'
+  end
+
+
+  private 
+
+  def pool
+    @pool = Pool.find(params[:pool_id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name,:flag_url,:pool_id)
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
+  validates :name, :flag_url, presence: true
   belongs_to :pool
 end
