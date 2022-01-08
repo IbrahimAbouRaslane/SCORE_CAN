ff0a80a6072ef9a50b65804594583d7003a01d1f
Djeneka <djeneka@MacBook-Pro-de-MacBook.local>
2022-01-08 18:43:57 +0000

    commit match

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
- 
   devise_for :users
+  resources :matches
+  resources :pools do 
+    resources :teams do
+      resources :matches
+    end
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
