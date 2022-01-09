f5adf377d0d398085b0eafaaeb233667136ee258
kaudaouda <konedaouda7894@yahoo.com>
2022-01-09 16:57:22 +0000

    devise config

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
-  root to: "home#index"
-  
-=======
   resources :pools do
     resources :teams
   end
->>>>>>> 2d5d73b89e74a5f249d19304503581a7cfec769c
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
